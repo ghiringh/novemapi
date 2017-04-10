8c06b4e2a29f6a3606a73714c3cb5d3931348f98
Pierre-Adrien Ghiringhelli <ghiringhelli.pierreadrien@gmail.com>
2017-04-10 11:07:28 +0200

    mongoose

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/transmedia');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 

065cac7e169ea7a983e9f710a6f3ec1b3057c376
Pierre-Adrien Ghiringhelli <ghiringhelli.pierreadrien@gmail.com>
2017-04-28 16:38:35 +0200

    old version etape

--- models/etape.js
+++ models/etape.js
@@ -20,20 +20,10 @@ const etapeSchema = new Schema({
 	}*/
 
 	propositions: {
-		niveau: Number,
-		question: String,
-		propositions: {
-			reponse: String,
-			score: {
-				business:Number,
-				coding: Number,
-				communication : Number,
-				management : Number,
-				marketing : Number,
-				multimedia: Number
-			},
-			competence: String
-		}
+		type: Array,
+		required: true,
+		minlength: [ 2, 'La proposition est trop courte' ],
+		maxlength: [ 1000, 'La proposition est trop longue' ]
 	},
 	
 	date_creation: {
